d8091a4d16b33d24346cdfb96534047eb04410ea
VIPadm <vipadm@vipadms-imac.local>
2018-04-25 15:38:11 +0530

    Initial Commit

---
+++ DemoJsonParsing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoJsonParsing
+//
+//  Created by VIPadm on 25/04/18.
+//  Copyright © 2018 botla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
