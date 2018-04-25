754e7e9ad5180996ebfab7c79ae6c7c510bc3173
VIPadm <vipadm@vipadms-imac.local>
2018-04-25 18:55:32 +0530

    json example

--- DemoJsonParsing/ViewController.m
+++ DemoJsonParsing/ViewController.m
@@ -8,21 +8,64 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () {
+    NSMutableDictionary *countriesDic;
+}
 
 @end
 
 @implementation ViewController
+@synthesize tableView;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    countriesDic = [[NSMutableDictionary alloc] init];
+    
+    NSString *url = @"http://services.groupkt.com/country/get/all";
+    
+    NSCharacterSet *set = [NSCharacterSet URLQueryAllowedCharacterSet];
+    NSString *encodedUrlString = [url stringByAddingPercentEncodingWithAllowedCharacters:set];
+    
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
+    
+    [[session dataTaskWithURL:[NSURL URLWithString:encodedUrlString] completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        
+        if (error != nil) {
+            NSLog(@"Error: %@", error.localizedDescription);
+        } else if ([response isKindOfClass:[NSHTTPURLResponse class]]){
+            NSError *jsonError;
+            NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+            NSLog(@"json: %@", json);
+            
+            if (jsonError != nil) {
+                NSLog(@"JsonError: %@", jsonError.localizedDescription);
+            } else {
+                countriesDic = [[json objectForKey:@"RestResponse"] objectForKey:@"result"];
+                NSLog(@"Countries Dic: %@", countriesDic);
+            }
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [tableView reloadData];
+            });
+        }
+        
+    }] resume];
+
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return countriesDic.count;
+}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSString *cellId = @"cellID";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellId];
+    }
+   
+    cell.textLabel.text = [[countriesDic valueForKey:@"name"] objectAtIndex:indexPath.row];
+    
+    return cell;
 }
 
 
